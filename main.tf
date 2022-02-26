751c6953b94bb3a6d23841db8a10a6085fd97235
Torsten Boettjer <57480261+torstenboettjer@users.noreply.github.com>
2022-02-26 12:00:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 // Copyright (c) 2020 Oracle and/or its affiliates.
 // Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 
-// readme.md created with https://terraform-docs.io/: terraform-docs markdown --sort=false ./ > ./readme.md
-
 // --- provider settings --- //
 terraform {
   required_providers {
