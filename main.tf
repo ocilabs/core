b2c76d0758e6139c14158f9eec62d7bb9531691f
Torsten Boettjer <torsten.boettjer@oracle.com>
2022-03-29 19:28:33 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ output "encryption" {
 }
 // --- wallet configuration --- //
 
-// --- network configuration --- //
+/*/ --- network configuration --- //
 module "network" {
   source = "github.com/ocilabs/network"
   depends_on = [module.configuration, module.resident]
@@ -141,7 +141,7 @@ output "database" {
   value = {for resource, parameter in module.database : resource => parameter}
   sensitive = true
 }
-// --- database creation --- //
+// --- database creation --- /*/
 
 
 /*/ --- host configuration --- //
